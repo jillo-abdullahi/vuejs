37aae9c346509e8197f827c890052ac3e4864434
Jillo Woche <jillo.abdulla@gmail.com>
2020-09-21 11:18:23 +0300

    using methods within Vue instance

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ new Vue({
   el: "#vue-app",
   data: {
     name: "Jillo Woche",
+    job: "Plumber"
   },
+  methods: {
+      greet: function(time){
+        return `Good ${time} ${this.name}!`
+      }
+  }
 });
