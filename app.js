215e72c49a2eeaa26acca8ab77e4868323790e51
Jillo Woche <jillo.abdulla@gmail.com>
2020-09-21 12:26:25 +0300

    introduction to events and their shorthand

--- app.js
+++ app.js
@@ -3,12 +3,25 @@ new Vue({
   data: {
     name: "Jillo Woche",
     job: "Software Developer",
+    age: 30,
     website: "https://jillowoche.dev",
     websiteTag: '<a href="https://jillowoche.dev">Tagged website</a>',
+    x: 0,
+    y: 0,
   },
   methods: {
     greet: function (time) {
       return `Good ${time} ${this.name}!`;
     },
+    addAge: function (inc) {
+      return this.age+=inc;
+    },
+    subtractAge: function (dec) {
+      return this.age-=dec;
+    },
+    updateXY: function(event){
+        this.x = event.offsetX;
+        this.y = event.offsetY;
+    }
   },
 });
